f26b7121fb7e273679f8e902506ffa188d12b425
MaxEpstein <max.epstein@hotmail.com>
2025-03-04 16:34:59 -0500

    base metrics completed, need to finish for other models and compute average across 100 for each

--- metrics.py
+++ metrics.py
@@ -7,3 +7,12 @@
 
 # Interpretability Metrics:
     # SHAP
+
+from sklearn.metrics import auc, accuracy_score, precision_score, recall_score, f1_score
+def getMetrics(x_val, y_val):
+    #areaUnder = auc(x_val, y_val)
+    accuracy = accuracy_score(x_val, y_val)
+    precision = precision_score(x_val, y_val)
+    recall = recall_score(x_val, y_val)
+    f1 = f1_score(x_val, y_val)
+    return  accuracy, precision, recall, f1
