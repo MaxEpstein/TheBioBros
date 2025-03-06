83cf9001b8211883face454acc3014e25295760d
MaxEpstein <max.epstein@hotmail.com>
2025-03-06 17:24:18 -0500

    updates to logistic regressions

--- modeling.py
+++ modeling.py
@@ -72,25 +72,26 @@ def model_adaboost(X_train, X_test, y_train, y_test, state=42):
 
 from sklearn.linear_model import LogisticRegression
 def model_logisticregression(X_train, X_test, y_train, y_test, state=42):
-    lr = LogisticRegression(random_state=state)
+    lr = LogisticRegression(random_state=state, solver='saga')
     lr.fit(X_train, y_train)
     y_pred = lr.predict(X_test)
     return y_pred, lr
 
 def model_lassoregularization(X_train, X_test, y_train, y_test, state=42):
-    lr1 = LogisticRegression(penalty = 'l1', random_state=state)
+    lr1 = LogisticRegression(penalty = 'l1', random_state=state, solver='saga')
     lr1.fit(X_train, y_train)
     y_pred = lr1.predict(X_test)
     return y_pred, lr1
 
 def model_ridgeRegularization(X_train, X_test, Y_train, Y_test, state=42):
-    lr2 = LogisticRegression(penalty = 'l2', random_state=state)
+    lr2 = LogisticRegression(penalty = 'l2', random_state=state, solver='saga')
     lr2.fit(X_train, Y_train)
     y_pred = lr2.predict(X_test)
     return y_pred, lr2
 
 def model_elasticNetRegularization(X_train, X_test, Y_train, Y_test, state=42):
-    lrE = LogisticRegression(penalty = 'elasticnet', random_state=state)
+    # TODO: Edit the l1 ratio for this. ratio = 0 is same is l1. ratio = 1 is same as l2
+    lrE = LogisticRegression(penalty = 'elasticnet', random_state=state, solver='saga', l1_ratio=0.5)
     lrE.fit(X_train, Y_train)
     y_pred = lrE.predict(X_test)
     return y_pred, lrE
