59b19c1ba9f680b9c5f5836256e21f12de61608a
MaxEpstein <max.epstein@hotmail.com>
2025-03-06 17:06:20 -0500

    Running all 17 models, ROC AUC metric working

--- metrics.py
+++ metrics.py
@@ -8,11 +8,12 @@
 # Interpretability Metrics:
     # SHAP
 
-from sklearn.metrics import auc, accuracy_score, precision_score, recall_score, f1_score
-def getMetrics(x_val, y_val):
-    #areaUnder = auc(x_val, y_val)
-    accuracy = accuracy_score(x_val, y_val)
-    precision = precision_score(x_val, y_val)
-    recall = recall_score(x_val, y_val)
-    f1 = f1_score(x_val, y_val)
-    return  accuracy, precision, recall, f1
+from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, f1_score
+def getMetrics(y_test, x_test, y_pred, model):
+    y_prob = model.predict_proba(x_test)[:,1]
+    areaUnder = roc_auc_score(y_test, y_prob)
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    return  areaUnder, accuracy, precision, recall, f1
