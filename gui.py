466fc54c0d41579c7273ac935d5930fa62adf85d
L-Spratt <lspratt007@gmail.com>
2025-03-14 22:13:57 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -11,14 +11,32 @@ def hello_world():
 <style>
 h1 {text-align: center;}
 h3 {text-align: center;}
+div {text-align: center;}
 </style>
 </head>
 <body>
 
 <h1>GutCheck</h1>
 <h3>Microbiome ML Pipeline for Analysis and Interpretation</h3>
-<p>This is a paragraph.</p>
-<div>This is a div.</div>
+<div><button type="button">Upload Dataset</button></div>
+<h3>Preprocessing</h3>
+<div><table align="center" style="margin: 0px auto;" style="background-color: #808080;">
+  <tr>
+    <th>Preprocessing Step</th>
+    <th>Progress</th>
+  </tr>
+  <tr>
+    <td>Train-Test Split</td>
+    <td>✔️ Success</td>
+  </tr>
+  <tr>
+    <td>Feature Selection</td>
+    <td>In Progress...</td>
+  </tr>
+</table></div>
+<h3>Model Training</h3>
+<h3>Model Evaluation</h3>
+<h3>Model Interpretation</h3>
 
 </body>
 </html>'''
