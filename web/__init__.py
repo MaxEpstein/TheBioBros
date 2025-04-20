9911cf65955184651ce61b808ee0858e2bd96334
L-Spratt <lspratt007@gmail.com>
2025-04-20 12:47:02 -0400

    update

--- web/__init__.py
+++ web/__init__.py
@@ -2,20 +2,33 @@ from flask import Flask, render_template, request
 import csv, io
 from main import pipeline
 import pandas as pd
+import base64
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_world():
     if request.method == 'POST':
-        file = request.files['filename']
-        if file:
-            df = pd.read_csv(file)
+        rare = request.files['filerare']
+        clr = request.files['fileclr']
+        if rare:
+            df = pd.read_csv(rare)
             print(df.head())
             df = df.drop(columns = ['Diagnosis'])
-            _, _, _, _, _, model_name = pipeline(df.iloc[:,1:].to_numpy())
-            print(model_name)
-        return render_template('index.html')
+            _, _, _, _, _, rare_model_name, rare_metrics, rare_interpret, rare_list = pipeline(df.iloc[:,1:].to_numpy())
+            print(rare_model_name)
+            rare_image_metrics = rare_metrics
+        if clr:
+            df = pd.read_csv(clr)
+            print(df.head())
+            df = df.drop(columns = ['Diagnosis'])
+            _, _, _, _, _, clr_model_name, clr_metrics, clr_interpret, clr_list = pipeline(df.iloc[:,1:].to_numpy())
+            print(clr_model_name)
+        return render_template('submitted.html', rare_model_name=rare_model_name, clr_model_name=clr_model_name,
+                               rare_metrics=rare_metrics, rare_interpret=rare_interpret, rare_list=rare_list, 
+                               clr_metrics=clr_metrics, clr_interpret=clr_interpret, clr_list=clr_list)
     if request.method == 'GET':
         return render_template('index.html')
     return render_template('index.html')
